31725a56ed86b4ef5f467777703df95da157787e
Ankit Sahay <139492522+thebrokenapp@users.noreply.github.com>
2023-10-25 00:33:09 +0530

    Sending data asynchronously to Kafka

---
+++ python/async_producer_with_callback.py
@@ -0,0 +1,25 @@
+from confluent_kafka import Producer
+
+conf = {
+          'bootstrap.servers': 'localhost:9092'
+       }
+
+def delivery_callback(err, msg):
+  if err:
+    print("Error occurred: {}".format(err))
+  else:
+    print("Msg sent to topic {topic} with key as {key} & value as {value} and sent to partition {partition} & written to offset {offset}".format(
+      topic = msg.topic(), key = msg.key().decode('utf-8'), value = msg.value().decode('utf-8'), partition = msg.partition(), offset = msg.offset()))
+
+producer = Producer(conf)
+topic = 'sales-topic'
+# note how messages are sent in order
+for i in range(100):
+  producer.produce(topic, key="key" + str(i), value="value" + str(i), callback = delivery_callback)
+  if i%20 == 0:
+    producer.flush()
+
+producer.flush()
+                  
+
+#bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic sales-topic --formatter kafka.tools.DefaultMessageFormatter --property print.timestamp=true --property print.key=true --property print.value=true  --property print.partition=true
